c6890c63eefd8868cdf80417bac5778cd15c2f80
Aset Madraimov <xiaset@gmail.com>
2024-05-04 01:08:41 +0500

    fix compute instance versions

--- compute-instance/main.tf
+++ compute-instance/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  # The latest version of Terragrunt (v0.29.0 and above) recommends Terraform 0.15.0 or above.
-  required_version = "~> 1.0.0"
-
-  # Live modules pin exact provider version; generic modules let consumers pin the version.
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.80.0"
-    }
-  }
-}
-
 resource "yandex_compute_instance" "this" {
   name        = var.name
   description = var.description
